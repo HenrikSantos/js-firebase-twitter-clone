819ab0093b8f4e1e93da614466ce1d1c1e8c439b
Henrik Santos <henrik.ruan4@gmail.com>
2024-03-22 14:35:47 -0300

    🐛 eslint

--- src/components/CommentForm/CommentForm.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CommentForm from "./CommentForm";
-
-test("checks if the div with text CommentForm is present", () => {
-  const { getByText } = render(<CommentForm />);
-  const divElement = getByText(/CommentForm/i);
-  expect(divElement).toBeInTheDocument();
-});
--- src/components/PostForm/PostForm.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import PostForm from "./PostForm";
-
-test("checks if the div with text PostForm is present", () => {
-    const { getByText } = render(<PostForm />);
-    const divElement = getByText(/PostForm/i);
-    expect(divElement).toBeInTheDocument();
-});
